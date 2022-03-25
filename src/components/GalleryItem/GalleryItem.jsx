2747cc4990e76a15b76feab50746cb956436fe12
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-25 16:07:57 -0500

    add like button click event

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,16 @@
 function GalleryItem ({item}) {
 
+    const addLike = () => {
+        console.log('CLICK');
+
+    }
+
     return (
         <div>
             <img src={item.path}/>
+            <br />
+            <button onClick={addLike}>I LIKEY</button>
+            <p>AMOUNT OF LIKES HERE</p>
         </div>
     )
 };
