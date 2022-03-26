cff9953f6f3d0badd48001cd7f7eaaa11ddb598d
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-26 13:02:38 -0500

    add toggle picture to description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,28 +1,58 @@
 import Axios from "axios";
-import {useState} from 'react'
+import { useState } from 'react'
 
-function GalleryItem ({item, getGallery}) {
+function GalleryItem({ item, getGallery }) {
 
-    const [showDescription, setShowDescription] = useState(true)
+    const [showDescription, setShowDescription] = useState(false)
 
     const addLike = () => {
         console.log('CLICK');
         Axios.put(`/gallery/like/${item.id}`)
-        .then(response => {
-            getGallery();
-            console.log('Liked', item);
-        }).catch(err => {
-            console.log('Error in liking', err);
-        })
+            .then(response => {
+                getGallery();
+                console.log('Liked', item);
+            }).catch(err => {
+                console.log('Error in liking', err);
+            })
 
     }
 
+    const handleToggleStatus = () => {
+        console.log('TOGGLE');
+        setShowDescription(!showDescription);
+    }
+
+    const handleToggle = () => {
+        if (showDescription === false) {
+            return (
+                <>
+                    <div>
+                        <img src={item.path} onClick={handleToggleStatus} />
+                    </div>
+                    <br />
+                    <button onClick={addLike}>LIKE</button>
+                    <p>{item.likes} people like this!</p>
+                </>
+            )
+        } else if (showDescription === true) {
+            return (
+                <>
+                    <div className="mimicImage" onClick={handleToggleStatus}>
+                        <p>{item.description}</p>
+                    </div>
+                    <br />
+                    <button onClick={addLike}>LIKE</button>
+                    <p>{item.likes} people like this!</p>
+                </>
+
+            )
+        }
+    }
+
+
     return (
         <div className="container">
-            <img src={item.path}/>
-            <br />
-            <button onClick={addLike}>I LIKEY</button>
-            <p>{item.likes} people like this!</p>
+            {handleToggle()}
         </div>
     )
 };
